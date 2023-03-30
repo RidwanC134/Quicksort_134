b35c2c0208fba08f15edbfdf41e9ee82bd7810b2
RidwanC134 <ridwanhh10@gmail.com>
2023-03-30 16:30:15 +0700

    membuat untuk perbandingan yang akan dilakukan

--- Quicksort_134/Quicksort_134.cpp
+++ Quicksort_134/Quicksort_134.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 int arr[20];                            // array of integers to hold values 
-int cmp_count = 0;                      // number of comparasion
+int cmp_count = 0;                      // number of comparasion 
 int mov_count = 0;                      // number of data movements
 int n;
 
